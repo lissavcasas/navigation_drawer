1ba077e3881fd07ed232e722260cb8277d9cdcd5
marimeli <marimel.casas@gmail.com>
2020-02-03 12:52:37 -0500

    minor changes

--- lib/home.dart
+++ lib/home.dart
@@ -1,38 +1,4 @@
 import 'package:flutter/material.dart';
- 
-/* class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  } */
-
-  
 
 class Home extends StatelessWidget {
 @override
