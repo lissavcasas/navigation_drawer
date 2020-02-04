82444532c56464915d2de2facd7792149e84ac89
marimeli <marimel.casas@gmail.com>
2020-02-04 13:01:41 -0500

    navigator push

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:menu_navigation/src/pages/alert_page.dart';
+import 'package:menu_navigation/src/pages/avatar_page.dart';
 import 'package:menu_navigation/src/pages/home_page.dart';
 /* import 'package:menu_navigation/src/pages/home_temp.dart';
 import 'navigation.dart'; */
@@ -11,14 +13,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: _title,
-        theme: ThemeData(
-          primaryColor: Colors.red[800],
-        ),
-        //home: Navigation(),
-        home: HomePage(),
-        //home: HomeTemp(),
-        );
+      debugShowCheckedModeBanner: false,
+      title: _title,
+      theme: ThemeData(
+        primaryColor: Colors.red[800],
+      ),
+      //home: Navigation(),
+      home: HomePage(),
+      //home: HomeTemp(),
+      /* initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        '/alert': (BuildContext context) => AlertPage(),
+        '/avatar': (BuildContext context) => AvatarPage(),
+      }, */
+    );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:menu_navigation/src/providers/menu_provider.dart';
 import 'package:menu_navigation/src/utils/icono_string_util.dart';
+import 'package:menu_navigation/src/pages/alert_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -16,39 +17,34 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    //print(menuProvider.options);
-    /* menuProvider.loadData().then((options){
-     print('options $options');
-   });
- */
-
     return FutureBuilder(
       future: menuProvider.loadData(),
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _itemsList(snapshot.data),
+          children: _itemsList(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _itemsList(List<dynamic> data) {
-
+  List<Widget> _itemsList(List<dynamic> data, BuildContext context) {
     final List<Widget> options = [];
 
-    data.forEach((opt){
+    data.forEach((opt) {
       final widgetTemp = ListTile(
         title: Text(opt['texto']),
         //leading: Icon(Icons.kitchen, color: Colors.red[800]),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.red[800]),
         onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+          Navigator.push(context, route);
 
+          //Navigator.pushNamed(context, opt['ruta']);
         },
       );
-      options..add(widgetTemp)
-             ..add(Divider());
+      options..add(widgetTemp)..add(Divider());
     });
 
     return options;
